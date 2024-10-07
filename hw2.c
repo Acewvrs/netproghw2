6c6f109f410e3c2a627e90ae955ba5e7b7f991a3
Jun <song.junyoung64@gmail.com>
2024-10-06 22:32:16 -0400

    let client connect

--- hw2.c
+++ hw2.c
@@ -8,10 +8,10 @@
 #include "../../lib/unp.h"
 // #include "unp.h" 
 
+// find the length the of the string, ignore all NULL characters
 int stringSize(char *str) {
     int length = 0;
     char* ptr = str;
-    // Loop till the NULL character is found
     while (*ptr != '\0' && *ptr != '\n' && *ptr != '\r' && *ptr) {
         length++;
         ptr++;
@@ -20,7 +20,6 @@ int stringSize(char *str) {
 }
 
 void tolower_string(char* str) {
-    printf("%s", str);
     for(int i = 0; *(str+i); i++){
         *(str + i) = tolower(*(str + i));
         // printf(" %c ", *(str + i));
@@ -58,19 +57,14 @@ int main(int argc, char ** argv ) {
         num_words++;
     }
 
-    char* test = "test";
-    printf("len: %d \n", stringSize(test));
     // save all valid words
     char** dict = calloc(num_words, sizeof(char*));
-
-    printf("%d \n", num_words);
 
     // reset file pointer
     fseek(file, 0, SEEK_SET);
 
     int dict_idx = 0;
     while (fgets(word, MAX_LEN, file)) {
-        // if (strlen(word) != 5) continue; 
         tolower_string(word);
         *(dict+dict_idx) = calloc(MAX_LEN, sizeof(char));
         *(word + stringSize(word)) = '\0';
@@ -84,81 +78,88 @@ int main(int argc, char ** argv ) {
     // set random seed
     srand( seed );
 
+    char* hidden_word = dict[rand() % num_words];
+
+    // printf("hidden: %s", hidden_word);
     // #=============================================================================
-    // int server_socks[5] = {0, 0, 0, 0, 0};
+    int server_socks[5] = {0, 0, 0, 0, 0};
     // int server_ports[5] = {0, 0, 0, 0, 0};
+    int num_connected = 0;
 
-    // fd_set		readfds, reads;
+    fd_set readfds, reads;
 
-    // // Initialize the file descriptor set
-    // FD_ZERO(&readfds);
-    // FD_SET(STDIN_FILENO, &reads); // Add stdin to set
+    // Initialize the file descriptor set
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &reads); // Add stdin to set
 
-    // while (1) {
-    //     readfds = reads;
+    int					sockfd;
+    struct sockaddr_in	servaddr;
+    // connect to the server port from STDIN
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    //     // Check if any of the connected servers have sent data
-    //     int num_ready = select(8, &readfds, NULL, NULL, NULL);
-    //     if (num_ready < 0) {
-    //         perror("select error");
-    //         exit(1);
-    //     }
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family      = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port        = htons(port);
 
-    //     if (num_connected < 5 && FD_ISSET(STDIN_FILENO, &readfds)) { // 
-    //         int					sockfd;
-    //         struct sockaddr_in	servaddr;
-    //         int serv_port;
-    //         Readline(STDIN_FILENO, port_input, MAXLINE);
-    //         serv_port = atoi(port_input);
+    while (1) {
+        readfds = reads;
 
-    //         // connect to the server port from STDIN
-    //         sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+        // Check if any of the connected servers have sent data
+        int num_ready = select(8, &readfds, NULL, NULL, NULL);
+        if (num_ready < 0) {
+            perror("select error");
+            exit(1);
+        }
 
-    //         bzero(&servaddr, sizeof(servaddr));
-    //         servaddr.sin_family      = AF_INET;
-    //         servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    //         servaddr.sin_port        = htons(serv_port);
+        if (num_connected < 5) { 
+            // Readline(STDIN_FILENO, port_input, MAXLINE);
+            // serv_port = atoi(port);
 
-    //         if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
-    //             // printf("Failed to connect to port %d\n", serv_port);
-    //             close(sockfd);
-    //         } else {
-    //             // printf("Connected to port %d\n", serv_port);
-    //             // find the empty socket pos
-    //             for (int i = 0; i < 5; i++) {
-    //                 if (server_socks[i] == 0) {
-    //                     server_socks[i] = sockfd;
-    //                     server_ports[i] = serv_port;
-    //                     break;
-    //                 }
-    //             }
+            if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+                // printf("Failed to connect to port %d\n", serv_port);
+                close(sockfd);
+            } else {
+                // printf("Connected to port %d\n", serv_port);
+                char* msg = "Welcome to Guess the Word, please enter your username.\n";
+                int len = sizeof(servaddr);
+                // Sendto(sockfd, msg, strlen(msg), 0, (struct sockaddr *)&servaddr, &len);
+                send(sockfd, msg, strlen(msg), 0);
+                // find the empty socket pos
+                for (int i = 0; i < 5; i++) {
+                    if (server_socks[i] == 0) {
+                        server_socks[i] = sockfd;
+                        // server_ports[i] = port;
+                        break;
+                    }
+                }
 
-    //             FD_SET(STDIN_FILENO, &reads);
-    //             FD_SET(sockfd, &reads);
-    //             num_connected++;
-    //         }
-    //     }
+                FD_SET(STDIN_FILENO, &reads);
+                FD_SET(sockfd, &reads);
+                num_connected++;
+            }
+        }
 
         
-    //     for (int i = 0; i < 5; i++) {
-    //         if (server_socks[i] > 0 && FD_ISSET(server_socks[i], &readfds)) {
-    //             //  at least one server sent a message
-    //             int n = recv(server_socks[i], buffer, MAXLINE - 1, 0);
-    //             if (n == 0) {
-    //                 // Server closed connection
-    //                 printf("Server on %d closed\n", server_ports[i]);
-    //                 FD_CLR(server_socks[i], &reads);
-    //                 close(server_socks[i]);
-    //                 server_socks[i] = 0;
-    //                 server_ports[i] = 0;
-    //                 num_connected--;
-    //             } else if (n > 0) {
-    //                 buffer[n] = '\0';
-    //                 printf("%d %s", server_ports[i], buffer);
-    //             }
-    //         }
-    //     }
-    // }
+        // for (int i = 0; i < 5; i++) {
+        //     if (server_socks[i] > 0 && FD_ISSET(server_socks[i], &readfds)) {
+        //         //  at least one server sent a message
+        //         int n = recv(server_socks[i], buffer, MAXLINE - 1, 0);
+        //         if (n == 0) {
+        //             // Server closed connection
+        //             // printf("Server on %d closed\n", server_ports[i]);
+        //             FD_CLR(server_socks[i], &reads);
+        //             close(server_socks[i]);
+        //             server_socks[i] = 0;
+        //             // server_ports[i] = 0;
+        //             num_connected--;
+        //         } else if (n > 0) {
+        //             buffer[n] = '\0';
+        //             printf("%d %s", server_ports[i], buffer);
+        //         }
+        //     }
+        // }
+    }
 
     // /* ==================== network setup code above ===================== */
     // running = true;
